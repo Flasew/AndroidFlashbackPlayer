463ecd0d6a9aa4154f229964e2ac44907920cb9c
Flasew <af141_l@icloud.com>
2018-02-18 22:07:26 -0800

    Modified all the espreso tests

--- app/src/androidTest/java/edu/ucsd/team6flashbackplayer/FBPageTest.java
+++ app/src/androidTest/java/edu/ucsd/team6flashbackplayer/FBPageTest.java
@@ -12,89 +12,100 @@ import android.view.ViewParent;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withParent;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.is;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
-public class LikeDislikeTest {
+public class FBPageTest {
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
     @Test
-    public void likeDislikeTest() {
+    public void fBPageTest() {
         ViewInteraction appCompatButton = onView(
-                allOf(withId(R.id.main_songs), withText("Songs"),
+                allOf(withId(R.id.fb_button), withText("FLASHBACK"),
                         childAtPosition(
                                 allOf(withId(R.id.main_layout),
                                         childAtPosition(
                                                 withClassName(is("android.support.constraint.ConstraintLayout")),
                                                 0)),
-                                3),
+                                2),
                         isDisplayed()));
         appCompatButton.perform(click());
 
-        ViewInteraction appCompatImageButton = onView(
-                allOf(withId(R.id.like_button),
-                        childAtPosition(
-                                allOf(withId(R.id.song_entry),
-                                        withParent(withId(R.id.song_list))),
-                                2),
-                        isDisplayed()));
-        appCompatImageButton.perform(click());
+        ViewInteraction button = onView(
+                allOf(withId(R.id.fb_button)
+                        ));
+        button.check(matches(isDisplayed()));
 
-        ViewInteraction imageButton = onView(
-                allOf(withId(R.id.like_button),
-                        childAtPosition(
-                                allOf(withId(R.id.song_entry),
-                                        childAtPosition(
-                                                withId(R.id.song_list),
-                                                0)),
-                                1),
-                        isDisplayed()));
+        ViewInteraction textView = onView(allOf(withId(R.id.song_name)));
+        textView.check(matches(isDisplayed()));
+
+        ViewInteraction textView2 = onView(allOf(withId(R.id.song_artist)));
+        textView2.check(matches(isDisplayed()));
+
+        ViewInteraction imageButton = onView(allOf(withId(R.id.like_button)));
         imageButton.check(matches(isDisplayed()));
 
-        ViewInteraction appCompatImageButton2 = onView(
-                allOf(withId(R.id.like_button),
-                        childAtPosition(
-                                allOf(withId(R.id.song_entry),
-                                        withParent(withId(R.id.song_list))),
-                                2),
-                        isDisplayed()));
-        appCompatImageButton2.perform(click());
-
-        ViewInteraction appCompatImageButton3 = onView(
-                allOf(withId(R.id.dislike_button),
-                        childAtPosition(
-                                allOf(withId(R.id.song_entry),
-                                        withParent(withId(R.id.song_list))),
-                                1),
-                        isDisplayed()));
-        appCompatImageButton3.perform(click());
-
-        ViewInteraction imageButton2 = onView(
-                allOf(withId(R.id.dislike_button),
-                        childAtPosition(
-                                allOf(withId(R.id.song_entry),
-                                        childAtPosition(
-                                                withId(R.id.song_list),
-                                                0)),
-                                2),
-                        isDisplayed()));
+        ViewInteraction imageButton2 = onView(allOf(withId(R.id.dislike_button)));
         imageButton2.check(matches(isDisplayed()));
+
+        ViewInteraction textView3 = onView(
+                allOf(withId(R.id.location_lab), withText("Location:")));
+        textView3.check(matches(withText("Location:")));
+
+        ViewInteraction textView4 = onView(
+                allOf(withId(R.id.time_lab), withText("Time:")));
+        textView4.check(matches(withText("Time:")));
+
+        ViewInteraction button2 = onView(
+                allOf(withId(R.id.show_playlist)));
+        button2.check(matches(isDisplayed()));
+
+        ViewInteraction textView5 = onView(
+                allOf(withId(R.id.time_date_txt)));
+        textView5.check(matches(isDisplayed()));
+
+        ViewInteraction textView6 = onView(
+                allOf(withId(R.id.time_clock_txt), withText("---")));
+        textView6.check(matches(isDisplayed()));
+
+        ViewInteraction textView7 = onView(
+                allOf(withId(R.id.time_clock_txt), withText("---")));
+        textView7.check(matches(isDisplayed()));
+
+        ViewInteraction appCompatButton2 = onView(
+                allOf(withId(R.id.fb_button), withText("FLASHBACK")));
+        appCompatButton2.perform(click());
+
+        ViewInteraction textView8 = onView(
+                allOf(withText("Currently Playing")));
+        textView8.check(matches(withText("Currently Playing")));
+
+        ViewInteraction textView9 = onView(
+                allOf(withText("Currently Playing")));
+        textView9.check(matches(withText("Currently Playing")));
+
+        pressBack();
+
+        ViewInteraction button3 = onView(
+                allOf(withId(R.id.main_songs)));
+        button3.check(matches(isDisplayed()));
 
     }
 
